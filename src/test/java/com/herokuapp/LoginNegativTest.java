3f4246f9e8be605711b5e484503672173976f506
Marius <marius.amz1@yahoo.com>
2024-01-14 18:38:30 +0200

    14.01.2024

--- src/test/java/com/herokuapp/LoginNegativTest.java
+++ src/test/java/com/herokuapp/LoginNegativTest.java
@@ -1,2 +1,99 @@
-package com.herokuapp;public class LoginNegativTest {
+package com.herokuapp;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class LoginNegativTest {
+    private WebDriver driver;
+
+    @BeforeTest
+    public void setUp(){
+    driver = new ChromeDriver();
+    driver.get("https://the-internet.herokuapp.com/");
+    driver.manage().window().maximize();
+
+    //2. click pe Form Authentication
+        WebElement linkTextFormAuthentication = driver.findElement(By.linkText("Form Authentication"));
+        linkTextFormAuthentication.click();
+
+    }
+    public void LoginMethod(String inputUsername, String inputPassword ){
+
+        //3. click username & enter user : Marius
+        WebElement userNameInput = driver.findElement(By.id("username"));
+        userNameInput.sendKeys(inputUsername);
+
+        //4. click username & enter password : 123456789
+        WebElement passwordInput = driver.findElement(By.id("password"));
+        passwordInput.sendKeys(inputPassword);
+
+        // 5. click login button
+        WebElement loginButton = driver.findElement(By.className("radius"));
+        loginButton.click();
+
+        sleep(1000);
+
+    }
+    @Test(priority = 1)
+    @Parameters({"wrongUsername","correctPassword"})
+    public void WrongUsernameCorrectPassword(String wrongUsername,String correctPassword){
+
+        LoginMethod(wrongUsername,correctPassword);
+
+        // 6. Expected result: Your username is invalid!
+        WebElement subHeader = driver.findElement(By.id("flash"));
+        String actualSubHeader = subHeader.getText();
+        String expectedSubheader = "Your username is invalid!";
+        Assert.assertTrue(actualSubHeader.contains(expectedSubheader));
+
+        sleep(1000);
+    }
+    @Test(priority = 2)
+    @Parameters({"correctUsername","wrongPassword"})
+    public void CorrectUsernameWrongPassword(String correctUsername, String wrongPassword){
+
+        LoginMethod(correctUsername,wrongPassword);
+
+        // 6. Expected result: Your username is invalid!
+        WebElement subHeader = driver.findElement(By.id("flash"));
+        String actualSubHeader = subHeader.getText();
+        String expectedSubheader = "Your password is invalid!";
+        Assert.assertTrue(actualSubHeader.contains(expectedSubheader));
+
+        sleep(1000);
+    }
+    @Test(priority = 3)
+    @Parameters({"wrongUsername","wrongPassword"})
+    public void WrongUsernameWrongPassword(String wrongUsername, String wrongPassword){
+
+        LoginMethod(wrongUsername, wrongPassword);
+
+        // 6. Expected result: Your username is invalid!
+        WebElement subHeader = driver.findElement(By.id("flash"));
+        String actualSubHeader = subHeader.getText();
+        String expectedSubheader = "Your username is invalid!";
+        Assert.assertTrue(actualSubHeader.contains(expectedSubheader));
+
+        sleep(1000);
+    }
+
+    @AfterTest
+    public void teadDown(){
+        driver.quit();
+    }
+
+    public static void sleep(int miliseconds){
+        try {
+            Thread.sleep(miliseconds);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
--- src/test/java/com/herokuapp/LoginTest.java
+++ src/test/java/com/herokuapp/LoginTest.java
@@ -1,2 +1,66 @@
-package com.herokuapp;public class LoginTest {
+package com.herokuapp;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+    @Test(priority = 1)
+    public void login(){
+
+        // 1. deschide pagina Form Authentication
+        WebDriver driver = new ChromeDriver();
+
+        System.out.println("Deschide pagina");
+        driver.get("https://the-internet.herokuapp.com/");
+        driver.manage().window().maximize();
+
+        System.out.println("Asteapta 3 secunde");
+        sleep(2000);
+
+        WebElement loginLink = driver.findElement(By.xpath("/html/body/div[2]/div/ul/li[21]/a"));
+        loginLink.click();
+
+        // 2. click username & enter user : tomsmith
+        WebElement usernameInput = driver.findElement(By.id("username"));
+        usernameInput.sendKeys("tomsmith");
+
+        // 3. click password & enter: SuperSecretPassword
+        WebElement passwordInput = driver.findElement(By.id("password"));
+        passwordInput.sendKeys("SuperSecretPassword!");
+
+        // 4. click login button
+        WebElement loginButton = driver.findElement(By.className("radius"));
+        loginButton.click();
+
+        // 5. Expected result: Welcome to the Secure Area
+        WebElement securedAreaSubheader = driver.findElement(By.className("subheader"));
+        String subheaderContent = "Welcome to the Secure Area. When you are done click logout below.";
+
+        Assert.assertTrue(securedAreaSubheader.isDisplayed());
+        Assert.assertEquals(subheaderContent,securedAreaSubheader.getText());
+
+        String secureURL = "https://the-internet.herokuapp.com/secure";
+        String currentURL = driver.getCurrentUrl();
+        Assert.assertEquals(currentURL,secureURL);
+
+        WebElement succesMessage = driver.findElement(By.id("flash"));
+        String succesMessageContent = "You logged into a secure area!";
+        Assert.assertTrue(succesMessage.getText().contains(succesMessageContent));
+
+        System.out.println("Inchide pagina");
+        driver.quit();
+    }
+
+    public static void sleep(int miliseconds){
+        try {
+            Thread.sleep(miliseconds);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
